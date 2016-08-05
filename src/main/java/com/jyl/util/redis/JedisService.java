3d138772009db56ff7faba47642e8d3f3379d74f
long <jyl0401@163.com>
2016-08-05 14:28:57 +0800

    add redis service.
    Signed-off-by: long <jyl0401@163.com>

--- src/main/java/com/jyl/util/redis/JedisService.java
+++ src/main/java/com/jyl/util/redis/JedisService.java
@@ -39,7 +39,7 @@ public class JedisService {
 		JedisPoolConfig config = new JedisPoolConfig();
 		//控制一个pool可分配多少个jedis实例，通过pool.getResource()来获取；
 		//如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
-		config.setMaxTotal(30);;
+		config.setMaxTotal(30);
 		//控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
 		config.setMaxIdle(5);
 		//表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
@@ -67,7 +67,7 @@ public class JedisService {
 	 */
 	public void returnResource(Jedis redis) {
 	    if (redis != null) {
-	        pool.close();
+	        redis.close();
 	    }
 	}
 	
